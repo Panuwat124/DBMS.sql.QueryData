84abc67442d7a36c58571db0772e4e7791f4bd85
Panuwat124 <phanuwaths07@gmail.com>
2024-11-18 15:52:18 +0700

    dddddd

--- DBMS.sql.Querydata/Form1.Designer.cs
+++ DBMS.sql.Querydata/Form1.Designer.cs
@@ -29,6 +29,9 @@
         private void InitializeComponent()
         {
             dataGridView1 = new DataGridView();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
             ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
             SuspendLayout();
             // 
@@ -37,18 +40,51 @@
             dataGridView1.AllowUserToAddRows = false;
             dataGridView1.AllowUserToDeleteRows = false;
             dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridView1.Location = new Point(362, 87);
+            dataGridView1.Location = new Point(152, 131);
             dataGridView1.Name = "dataGridView1";
             dataGridView1.ReadOnly = true;
             dataGridView1.RowHeadersWidth = 51;
             dataGridView1.Size = new Size(404, 294);
             dataGridView1.TabIndex = 0;
             // 
+            // button1
+            // 
+            button1.Location = new Point(40, 35);
+            button1.Name = "button1";
+            button1.Size = new Size(209, 70);
+            button1.TabIndex = 1;
+            button1.Text = "พนักงาน";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(281, 36);
+            button2.Name = "button2";
+            button2.Size = new Size(225, 68);
+            button2.TabIndex = 2;
+            button2.Text = "หมวดหมู่สินค้า";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(533, 40);
+            button3.Name = "button3";
+            button3.Size = new Size(221, 65);
+            button3.TabIndex = 3;
+            button3.Text = "สินค้า";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
             Controls.Add(dataGridView1);
             Name = "Form1";
             Text = "DBMS.sql.QueryData";
@@ -60,5 +96,8 @@
         #endregion
 
         private DataGridView dataGridView1;
+        private Button button1;
+        private Button button2;
+        private Button button3;
     }
 }
--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,1 +1,3 @@
 select * from Products
+select * from Categories
+select EmployeeID,Title+FirstName+''+LastName EmpName,Position from Employees
